deb84700cbcfe72be605bec40a0e1c0d68d5461f
Daniels Umanovskis <du@axentia.se>
2019-08-21 11:21:22 +0200

    Cleanup to remove confusing variscite things

--- fix-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SRC=sources/poky/meta/recipes-core/initscripts/initscripts-1.0
-DST=build_x11/tmp/work/armv7at2hf-neon-fslc-linux-gnueabi/initscripts/1.0-r155
-
-for f in "$SRC"/*; do cp $f $DST; done
-cp $DST/GPLv2.patch $DST/COPYING
-sed -i "s/^+//g" $DST/COPYING
-sed -i "1,8d" $DST/COPYING
